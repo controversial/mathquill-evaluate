ace661d5c30d59ae69a05af9cd16fa284c1db55d
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-15 19:57:22 -0400

    Add robustness to serve.sh
    Eventually this will be fleshed out into a script that can run the the full example, which will automate the installation, building, and serving.

--- example/serve.sh
+++ example/serve.sh
@@ -1,1 +1,8 @@
-php -S localhost:8080 ../build/request.php
+pwdname=${PWD##*/} # From http://stackoverflow.com/q/1371261/4414003
+
+if [[ $pwdname -eq "example" ]] # We are inside the 'example' directory
+then
+  cd ..
+fi
+
+php -S localhost:8080 build/request.php
