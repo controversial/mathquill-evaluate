e27de94d3f2d42f4356343ec87a087d812dc51e3
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-15 21:42:14 -0400

    Fix typo in serve.sh
    Now it works as intended, instead of always going up a level

--- example/serve.sh
+++ example/serve.sh
@@ -1,6 +1,6 @@
 pwdname=${PWD##*/} # From http://stackoverflow.com/q/1371261/4414003
 
-if [[ $pwdname -eq "example" ]] # We are inside the 'example' directory
+if [[ $pwdname == "example" ]] # We are inside the 'example' directory
 then
   cd ..
 fi
