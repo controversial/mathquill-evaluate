ae35ea8585865b1b353b2498f2c8c66d6200f353
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-19 22:27:10 -0400

    Make getWAResult use new method

--- src/wa.js
+++ src/wa.js
@@ -6,8 +6,8 @@
 // Make a Wolfram|Alpha API call given the path to request.php, a query, and an
 // API key. endpoint should point to a local, running copy of `request.php` in
 // order to get around same-origin.
-function getWAResult(endpoint, query, key, callback) {
-  var url = endpoint + "?input=" + encodeURIComponent(query) + "&appid=" + key;
+function getWAResult(endpoint, params, callback) {
+  var url = constructParams(endpoint, params);
   requestPage(url, function(data) {
     callback(xmlParse(data));
   });
