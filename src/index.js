7420c7e7e380b94b71ae4d7dbb32a2762929fd7f
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-21 22:23:55 -0400

    Basic mathfield evaluation \o/

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-function evaluate (mathfield) {
-  
+// Try to evaluate the content of a MathField
+function evaluate (mathfield, config) {
+  console.log(mathfield);
+  // Set input to MathField's LaTeX
+  config.input = mathfield.latex();
+  // Default endpoint is powered by http://crossorigin.me/. This could very
+  // easily break, so it's good to specify your own endpoint if you can.
+  config.endpoint = config.endpoint || "http://crossorigin.me/http://api.wolframalpha.com/v2/query";
+
+  // Add an extra layer around the callback
+  var oldCallback = config.callback;
+  var newCallback = function(data) {
+    oldCallback(getResult(data));
+  };
+  config.callback = newCallback;
+
+  var result = getWAResult(config);
 }
