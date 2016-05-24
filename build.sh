b2719d27f0d467d5d96d5159be33acca64d5318b
Luke Deen Taylor <ldt@deentaylor.com>
2016-05-23 20:37:22 -0400

    How did I manage to put a Ç in the comment!

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ printf "Building... "
 rm -rf build # Remove any old copies
 mkdir build  # Create build folder
 
-# Çoncatenate all JavaScript files into build/mathquill-evaluate.js`
+# Concatenate all JavaScript files into build/mathquill-evaluate.js`
 cat src/*.js > build/mathquill-evaluate.js
 
 # Include PHP file
